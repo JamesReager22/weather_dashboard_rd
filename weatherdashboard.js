a455a4c7037394c1aba3a9308f678ce22fbb3092
JamesReager22 <90424281+JamesReager22@users.noreply.github.com>
2021-10-31 22:17:56 -0700

    Delete weatherdashboard.js

--- weatherdashboard.js
+++
@@ -1,6 +0,0 @@
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}')
-.then(response =>{
-    return response.json();
-}).then(data =>{
-    console.log(data);
-})
